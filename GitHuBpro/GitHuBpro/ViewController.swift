919093a769a32304ca8f245f141b09ead1abc7a6
Yaroslav Halivets <yarikgalivetc@gmail.com>
2018-03-10 15:43:45 +0200

    add viewWillAppear and print hi

---
+++ GitHuBpro/GitHuBpro/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GitHuBpro
+//
+//  Created by Yaroslav Halivets on 3/10/18.
+//  Copyright © 2018 Yaroslav Halivets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hi")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+     print("help")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
