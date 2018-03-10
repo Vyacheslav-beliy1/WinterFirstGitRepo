186c5fd12f90091acb7f31d06a8d6d2ffe6d9809
Vyacheslav-beliy1 <mister.white.2013@gmail.com>
2018-03-10 14:49:44 +0200

    temp commit;

--- gitHub/gitHub/ViewController.swift
+++ gitHub/gitHub/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hellp")
-        
+        //фыващфывадл
         print("hello Slava")
         // Do any additional setup after loading the view, typically from a nib.
     }
